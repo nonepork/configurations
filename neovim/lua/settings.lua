7d934f4acd4e3c98e361d87a208c667c5ab27bc1
nonepork <59335048+nonepork@users.noreply.github.com>
2023-11-04 02:04:49 +0800

    Update settings.lua

--- neovim/lua/settings.lua
+++ neovim/lua/settings.lua
@@ -8,27 +8,42 @@ vim.g.maplocalleader = " "
 
 -- Editor options
 
-vim.o.hlsearch = true           -- Set highlight on search
-vim.o.number = true             -- Line number in front of each line
-vim.o.clipboard = "unnamedplus" -- Uses the clipboard register for all operations except yank.
-vim.o.syntax = "on"             -- Enable syntax highlight stuff
-vim.o.autoindent = true         -- Copy indent from current line when starting a new line.
-vim.o.expandtab = true          -- In Insert mode: Use the appropriate number of spaces to insert a <Tab>.
-vim.o.shiftwidth = 2            -- Number of spaces to use for each step of (auto)indent.
-vim.o.tabstop = 2               -- Number of spaces that a <Tab> in the file counts for.
-vim.o.encoding = "utf-8"        -- Sets the character encoding used inside Vim.
-vim.o.fileencoding = "utf-8"    -- Sets the character encoding for the file of this buffer.
-vim.o.ruler = true              -- Show the line and column number of the cursor position, separated by a comma.
-vim.o.mouse = "a"               -- Enable the use of the mouse. "a" you can use on all modes
-vim.o.title = true              -- When on, the title of the window will be set to the value of 'titlestring'
-vim.o.hidden = true             -- When on a buffer becomes hidden when it is |abandon|ed
-vim.o.ttimeoutlen = 0           -- The time in milliseconds that is waited for a key code or mapped key sequence to complete.
-vim.o.wildmenu = true           -- When 'wildmenu' is on, command-line completion operates in an enhanced mode.
-vim.o.showcmd = true            -- Show (partial) command in the last line of the screen. Set this option off if your terminal is slow.
-vim.o.showmatch = true          -- When a bracket is inserted, briefly jump to the matching one.
-vim.o.inccommand = "split"      -- When nonempty, shows the effects of :substitute, :smagic, :snomagic and user commands with the :command-preview flag as you type.
-vim.o.splitbelow = "splitright" -- When on, splitting a window will put the new window below the current one
-vim.o.ignorecase = true         -- Case insensitive when searching..
-vim.o.smartcase = true          -- Unless there is capital letter in it
-vim.o.termguicolors = true      -- Colors
+vim.opt.number = true             -- Line number in front of each line
+
+vim.opt.clipboard = "unnamedplus" -- Uses the clipboard register for all operations except yank.
+
+vim.opt.syntax = "on"             -- Enable syntax highlight stuff
+
+vim.opt.smartindent = true        -- Let vim decides when to indent, when not to.
+vim.opt.tabstop = 2               -- Number of spaces that a <Tab> in the file counts for.
+vim.opt.softtabstop = 2           -- I dont know what this is to be honest, tell me if you know :)
+vim.opt.shiftwidth = 2            -- Number of spaces to use for each step of (auto)indent.
+vim.opt.expandtab = true          -- In Insert mode: Use the appropriate number of spaces to insert a <Tab>.
+
+vim.opt.encoding = "utf-8"        -- Sets the character encoding used inside Vim.
+vim.opt.fileencoding = "utf-8"    -- Sets the character encoding for the file of this buffer.
+
+vim.opt.wrap = false              -- Disable wrap lines
+
+vim.opt.hlsearch = true           -- Set don't highlight on search
+vim.opt.incsearch = true          -- Like regex but for vim
+
+vim.opt.scrolloff = 8             -- Pretty cool feature that don't let you lost in the world of code
+
+vim.opt.ruler = true              -- Show the line and column number of the cursor position, separated by a comma.
+vim.opt.mouse = "a"               -- Enable the use of the mouse. "a" you can use on all modes
+vim.opt.hidden = true             -- When on a buffer becomes hidden when it is |abandon|ed
+vim.opt.ttimeoutlen = 0           -- The time in milliseconds that is waited for a key code or mapped key sequence to complete.
+vim.opt.wildmenu = true           -- When 'wildmenu' is on, command-line completion operates in an enhanced mode.
+vim.opt.showcmd = true            -- Show (partial) command in the last line of the screen. Set this option off if your terminal is slow.
+vim.opt.showmatch = true          -- When a bracket is inserted, briefly jump to the matching one.
+vim.opt.inccommand = "split"      -- When nonempty, shows the effects of :substitute, :smagic, :snomagic and user commands with the :command-preview flag as you type.
+vim.opt.splitbelow = true         -- When on, splitting a window will put the new window below the current one
+
+vim.opt.ignorecase = true         -- Case insensitive when searching..
+vim.opt.smartcase = true          -- Unless there is capital letter in it
+
+vim.opt.updatetime = 50           -- Updatetime
+
+vim.opt.termguicolors = true      -- Colors
 vim.opt.guifont = "JetBrainsMono NFM:h12" -- Font, really long 'cuz the og one didn't work
