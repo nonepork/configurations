d632211701e3f519af5943f8d65b7627bbd11f74
nonepork <59335048+nonepork@users.noreply.github.com>
2023-11-03 23:44:13 +0800

    Initial commit

---
+++ neovim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function () 
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+        ensure_installed = { "c",
+                             "c_sharp",
+                             "cpp",
+                             "lua",
+                             "vim",
+                             "vimdoc",
+                             "query",
+                             "javascript",
+                             "typescript",
+                             "html",
+                             "css",
+                             "python"},
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },  
+      })
+  end
+}
