772ec683d127c88bcbc6cf86b8b86ba0825fef7a
nonepork <59335048+nonepork@users.noreply.github.com>
2023-12-25 23:02:00 +0800

    catppuccin modify

--- neovim/lua/plugins/colorscheme.lua
+++ neovim/lua/plugins/colorscheme.lua
@@ -1,10 +1,6 @@
 return {
-    {"catppuccin/nvim",
+    "catppuccin/nvim",
     name = "catppuccin",
     lazy = false, -- Load this before start up 
     priority = 1000, -- Load this before everything
-    config = function()
-        -- Set colorscheme in here although I don't know why
-        vim.cmd([[colorscheme catppuccin-frappe]])
-    end,},
 }
