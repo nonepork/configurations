819d840d6581647fddcfb8b0e62a20ea34a2fce1
nonepork <59335048+nonepork@users.noreply.github.com>
2024-01-08 16:44:53 +0800

    Disable highlight when > 100kb & Rainbow puke parenthesis

--- neovim/after/plugin/nvim-treesitter.lua
+++ neovim/after/plugin/nvim-treesitter.lua
@@ -1,3 +1,29 @@
+local rainbow = require 'rainbow-delimiters'
+
+require("rainbow-delimiters.setup").setup({
+    highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+    },
+})
+
+vim.cmd([[highlight RainbowDelimiterRed guifg=#FFADAD]])
+vim.cmd([[highlight RainbowDelimiterYellow guifg=#FFD6A5]])
+vim.cmd([[highlight RainbowDelimiterBlue guifg=#FDFFB6]])
+vim.cmd([[highlight RainbowDelimiterOrange guifg=#CAFFBF]])
+vim.cmd([[highlight RainbowDelimiterGreen guifg=#9BF6FF]])
+vim.cmd([[highlight RainbowDelimiterViolet guifg=#A0C4FF]])
+vim.cmd([[highlight RainbowDelimiterCyan guifg=#BDB2FF]])
+
+require("nvim-highlight-colors").setup({
+    render = 'background'
+})
+
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = { "c",
@@ -32,12 +58,10 @@ require'nvim-treesitter.configs'.setup {
             if ok and stats and stats.size > max_filesize then
                 vim.cmd([[HighlightColorsOff]])
                 vim.cmd([[IlluminatePause]])
+                vim.cmd([[syntax off]])
+                rainbow.disable()
                 return true
             end
         end,
     },
 }
-
-require("nvim-highlight-colors").setup({
-    render = 'background'
-})
--- neovim/after/plugin/transparent.lua
+++ neovim/after/plugin/transparent.lua
@@ -1,4 +1,4 @@
---require('transparent').clear_prefix('lualine')
+require('transparent').clear_prefix('lualine')
 require('transparent').clear_prefix('NvimTree')
 require("notify").setup({
   background_colour = "#000000",
