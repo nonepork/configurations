08d13da7b58439ffd04f86fa9c8ee65d4f614cf3
nonepork <59335048+nonepork@users.noreply.github.com>
2024-04-13 14:52:31 +0800

    Fix typo and stuff

--- neovim/lua/plugins/undotree.lua
+++ neovim/lua/plugins/undotree.lua
@@ -2,6 +2,6 @@ return {
     "https://github.com/mbbill/undotree",
     lazy = false,
     config = function()
-        vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle, { silent = true, desc = 'Toggle nvim tree' })
+        vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle, { silent = true, desc = 'Toggle undo tree' })
     end,
 }
