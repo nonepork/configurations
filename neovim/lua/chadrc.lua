d9532f963f95e5e998a0cd36e411a4ce20561351
nonepork <59335048+nonepork@users.noreply.github.com>
2024-10-03 17:57:49 +0800

    Not sure if I did this right but it works ̄ _(ツ)_/ ̄

--- neovim/lua/chadrc.lua
+++ neovim/lua/chadrc.lua
@@ -1,38 +1,39 @@
 -- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- Please read that file to know all available options :(
 
----@type ChadrcConfig
-local M = {}
-
-M.ui = {
-  ------------------------------- base46 -------------------------------------
-  -- hl = highlights
-  theme = "everblush",
-  hl_override = {
-    Comment = { italic = true },
-    ["@comment"] = { italic = true },
+local options = {
+  base46 = {
+    theme = "everblush", -- default theme
+    hl_override = {
+      Comment = { italic = true },
+      ["@comment"] = { italic = true },
+    },
   },
 
-  cmp = {
-    icons = true,
-    lspkind_text = true,
-    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-  },
+  ui = {
+    cmp = {
+      icons = true,
+      lspkind_text = true,
+      style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    },
 
-  telescope = { style = "borderless" },
+    telescope = { style = "borderless" },
 
-  ------------------------------- nvchad_ui modules -----------------------------
-  statusline = {
-    theme = "minimal",
-  },
+    ------------------------------- nvchad_ui modules -----------------------------
+    statusline = {
+      theme = "minimal",
+    },
 
-  -- lazyload it when there are 1+ buffers
-  tabufline = {
-    enabled = true,
-    lazyload = true,
-    order = { "treeOffset", "buffers", "tabs", "btns" },
-    modules = nil,
+    -- lazyload it when there are 1+ buffers
+    tabufline = {
+      enabled = true,
+      lazyload = true,
+      order = { "treeOffset", "buffers", "tabs", "btns" },
+      modules = nil,
+    },
   },
 }
 
-return M
+local status, chadrc = pcall(require, "chadrc")
+return vim.tbl_deep_extend("force", options, status and chadrc or {})
