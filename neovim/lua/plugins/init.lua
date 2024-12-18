c0f41538d76b9dbeae1517cebe49e25e05f0322d
nonepork <ptohrek@gmail.com>
2024-12-18 23:41:42 +0800

    Update init.lua

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -72,8 +72,13 @@ return {
 
   {
     "folke/todo-comments.nvim",
-    lazy = true,
     dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
   },
 
   {
