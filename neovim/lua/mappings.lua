485929f63f0abcc91754ad0a3d1a73f0fa7e3467
nonepork <ptohrek@gmail.com>
2025-07-22 00:13:02 +0800

    Added a sorta evil thing

--- neovim/lua/mappings.lua
+++ neovim/lua/mappings.lua
@@ -1,4 +1,8 @@
 -- [[ Keymaps ]]
+-- am i evil for this?
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 vim.keymap.set('n', '<Esc>', '<cmd>noh<CR>')
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
