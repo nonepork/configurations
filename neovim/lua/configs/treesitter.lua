a387b1342aef28bc30b6b139b32272d0f73027b3
nonepork <59335048+nonepork@users.noreply.github.com>
2026-03-02 02:10:52 +0800

    🔧 Now I know why

--- neovim/lua/configs/treesitter.lua
+++ neovim/lua/configs/treesitter.lua
@@ -1,24 +1,32 @@
--- NOTE: why does kickstart.nvim's code look like that
-return {
-  ensure_installed = {
-    'bash',
-    'c',
-    'diff',
-    'html',
-    'lua',
-    'luadoc',
-    'markdown',
-    'markdown_inline',
-    'query',
-    'vim',
-    'vimdoc',
-  },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = { 'ruby' },
-  },
-  indent = { enable = true, disable = { 'ruby', 'cpp' } },
-}
+return function()
+  -- NOTE: uncomment those lines to install parsers on startup, or use `:TSInstall <language>` to install them manually
+  -- local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  -- require('nvim-treesitter').install(parsers)
+  vim.api.nvim_create_autocmd('FileType', {
+    callback = function(args)
+      local buf, filetype = args.buf, args.match
+
+      local language = vim.treesitter.language.get_lang(filetype)
+      if not language then
+        return
+      end
+
+      -- check if parser exists and load it
+      if not vim.treesitter.language.add(language) then
+        return
+      end
+      -- enables syntax highlighting and other treesitter features
+      vim.treesitter.start(buf, language)
+
+      -- enables treesitter based folds
+      -- for more info on folds see `:help folds`
+      -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      -- vim.wo.foldmethod = 'expr'
+
+      -- enables treesitter based indentation
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })
+end
 
 -- vim: ts=2 sts=2 sw=2 et
